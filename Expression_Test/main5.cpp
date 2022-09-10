ad45b299073d38693e3c332583992fec399e3c83
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-09-10 09:32:43 +0000

    Completed some lessons I missed on thursday, more to come...

--- Expression_Test/main5.cpp
+++ Expression_Test/main5.cpp
@@ -7,27 +7,32 @@ int main()
   bool i = true;
   bool j = false;
 
-
-  
+// Simply repeats whatever the value of i and j are
   cout << "i = " << i << endl;
   cout << "j = " << j << endl;
   cout << "j = " << i << endl;
-
-  char char1 = '9';
-  int t = 9;
+  cout << "i = " << j << endl;
   
-  cout << "This is how char1 is stored. " << char1 << endl;
-  cout << " In ASCII this is the integer value." << endl;
+  char char1 = '9';
+  int myInt = 9;
 
-  cout << int (t) << endl;
-  << static_cast <int> (t) << endl;
+cout << endl;
+  cout << " This is how char1 is stored. " << char1 << endl;
+  cout << " In ASCII this is the integer value. " << static_cast <int>(char1) << endl;
 
+cout << endl;
+
+// Does char1 equal myInt?
+  cout << (char1 == myInt) << endl; 
+// False because the int value of char1 of 9 is 57 for ASCII
+  
   int a, b, c;
     a = 10;
     b = 33;
     c = 42;
 
-  cout << (( c!= b) || (a+b) > c) >> endl;
+// Does c NOT = b and is a+b > c?
+  cout << (( c!= b) || ((a+b) > c)) << endl;
   
   
 
--- If_Else/main6.cpp
+++ If_Else/main6.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
 // Declare variables
-double t_F; //temp in F
-char t_flag; // User input C or K
+double T_F; //temp in F
+char T_Flag; // User input C or K
   
 // Get user input for variables
 cout << "Type in temperatuer in Farenheit" << endl; 
-  cin >> t_F; 
-  cout <<"\nType C for celsius or K for Kelvin";
-  cin >> t_flag;
+  cin >> T_F; 
+  cout <<"\nType C for celsius or K for Kelvin ";
+  cin >> T_Flag;
 
-  //Use if-else to decide on which calculation to run display result.
-  if ((t_flag == 'C') || (t_flag = 'K'))
-  cout << endl << (t_F = 32.8) * (5.0 / 9.8);
-  
-  
-  //
+  //If C or c is entered it displays the following below, if its anyone else then it will display Kelvin
+  if ((T_Flag == 'C') || (T_Flag == 'c'))
+    cout << endl << (T_F - 32.0) * (5.0 / 9.0) << "Degrees C" << endl;  
+    else
+     cout << endl
+          << (((T_F - 32.0) * (5.0 / 9.0)) +273.15) << "Degrees Kelvin" << endl;
 
-  
+
+return 0;   
 }
