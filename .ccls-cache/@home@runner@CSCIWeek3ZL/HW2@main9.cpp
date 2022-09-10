0008245120957910c4e9d24c0f7a43c19292e678
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-09-10 10:58:02 +0000

    Started HW 2, focusing more on user inputs rather than a concrete value as it's simply more useful.

---
+++ .ccls-cache/@home@runner@CSCIWeek3ZL/HW2@main9.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main ()
+{
+// Initalize Variables
+  int X_1, X_2;
+  int Y_1, Y_2;
+
+  cout << 
+  
+  cout << "To Calculate the slope of two lines you need the ∆X and ∆Y" <<
+  "\nEnter the first X point and then the second X." << endl;
+  
+
+
+
+
+
+  
+}
---
+++ HW2/main9.cpp
@@ -0,0 +1,48 @@
+/*
+
+
+
+Equation = (y2 - y1)/(x2 - x1) = m
+*/ 
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+int main ()
+{
+// Initalize Variables
+  
+  cout << setw(40) << "Calculating Slopes" << endl;
+  
+  int x_1, x_2;
+  int y_1, y_2;
+  double m;
+
+  
+  // Prompt user for inputs
+  cout << "To Calculate the slope of two lines you need the ∆X and ∆Y" <<
+  "\nEnter the first X point and then the second X as whole numbers." << endl;
+
+    // Acquire x values
+    cin >> x_1;
+    cin >> x_2;
+
+  cout <<"\n Now enter the first Y point and then the second Y as whole numbers." << endl;
+
+    // Acquire y Values
+    cin >> y_1;
+    cin >> y_1;
+
+  // Prompts formula to user
+  cout << setw(40) << "\nUsing the formula (y2-y1)/(x2-x1) to find the slope m" << endl;
+    
+  
+
+
+
+
+
+  
+}
